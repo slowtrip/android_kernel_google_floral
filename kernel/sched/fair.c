be47c521b92189f2a4efce6da8485458eef0a0b0
Kazuki Hashimoto <kazukih@tuta.io>
2021-12-03 09:18:47 +0900

    Revert "sched/fair: Modify capacity margins for sm8150"
    This reverts commit 3cbb371b7a9c5fd567c6174499df1b56c327935d.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -194,17 +194,13 @@ unsigned int capacity_margin				= 1280;
 
 /* Migration margins */
 unsigned int sysctl_sched_capacity_margin_up[MAX_MARGIN_LEVELS] = {
-			1280, 1625
-}; /* ~20% margin for small, ~37% margin for big */
+			[0 ... MAX_MARGIN_LEVELS-1] = 1078}; /* ~5% margin */
 unsigned int sysctl_sched_capacity_margin_down[MAX_MARGIN_LEVELS] = {
-			1796, 1442
-}; /* ~43% margin for big, ~29% margin for big+ */
+			[0 ... MAX_MARGIN_LEVELS-1] = 1205}; /* ~15% margin */
 unsigned int sched_capacity_margin_up[NR_CPUS] = {
-			1280, 1280, 1280, 1280, 1625, 1625, 1625, 1024
-}; /* ~20% margin for small, ~37% for big, not used for big+  */
+			[0 ... NR_CPUS-1] = 1078}; /* ~5% margin */
 unsigned int sched_capacity_margin_down[NR_CPUS] = {
-			1024, 1024, 1024, 1024, 1796, 1796, 1796, 1442
-}; /* Not used for small, ~43% margin for big, ~29% for big+ */
+			[0 ... NR_CPUS-1] = 1205}; /* ~15% margin */
 
 #ifdef CONFIG_SCHED_WALT
 /* 1ms default for 20ms window size scaled to 1024 */
